d785d297faa1a449fd9a792e49a266648320818b
Kola Sokol <maiden.tech@gmail.com>
2024-09-19 11:48:43 +0000

    new file:   pages/_app.js

---
+++ pages/_app.js
@@ -0,0 +1,26 @@
+import { NextScript } from 'next/document';
+import Head from 'next/head';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        {/* Google Analytics */}
+        <NextScript async src="https://www.googletagmanager.com/gtag/js?id=G-35JQN469E9"></NextScript>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-35JQN469E9');
+            `,
+          }}
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,26 @@
 export default {
-    logo: <span>My Nextra Documentation</span>,
+    logo: <span>My Memorybook</span>,
     project: {
-      link: 'https://github.com/shuding/nextra'
+      link: 'https://github.com/dedkola'
+      
+    },
+    head: (
+      <>
+     
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content="Nextra" />
+        <meta property="og:description" content="The next site builder" />
+    
+      </>
+
+    ),
+    //add `Docs` to title
+    useNextSeoProps() {
+      return {
+        titleTemplate: '%s – Docs'
+      }
     }
-    // ... other theme options
+    
+
   }
+
