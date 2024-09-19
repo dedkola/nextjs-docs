14b7f84bd9abef225f118efe27bf4cd75f644fe5
Kola Sokol <maiden.tech@gmail.com>
2024-09-19 12:00:34 +0000

    modified:   pages/_app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,24 @@
-import { NextScript } from 'next/document';
-import Head from 'next/head';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        {/* Google Analytics */}
-        <NextScript async src="https://www.googletagmanager.com/gtag/js?id=G-35JQN469E9"></NextScript>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-35JQN469E91');
-            `,
-          }}
-        />
-      </Head>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-35JQN469E9"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-35JQN469E9');
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </>
   );
