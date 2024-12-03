c5f1085a46125045a39f24be1c5433b01b9b78d5
Kola Sokol <maiden.tech@gmail.com>
2024-12-03 13:57:23 +0200

    <SpeedInsights/>

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import Script from "next/script";
 import "../src/globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+        <SpeedInsights />
       <Script
         async
         src="https://www.googletagmanager.com/gtag/js?id=G-35JQN469E9"
